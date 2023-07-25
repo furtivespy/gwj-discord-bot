8c556628aeeb5b2f8c9a5a88d5e7d0b746dc3077
Will Sullivan <furtivespy@gmail.com>
2023-07-24 20:55:47 -0400

    even more logging

--- src/commands/slash/voiceCreate.js
+++ src/commands/slash/voiceCreate.js
@@ -1,4 +1,8 @@
-const { SlashCommandBuilder, ChannelType, PermissionsBitField } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  ChannelType,
+  PermissionsBitField,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,20 +15,44 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    const name = interaction.options.getString("name");
-    const guildCategory = "on-demand voice";
-    const category = interaction.guild.channels.cache.find(
-      (c) => c.name.toLowerCase() == `${guildCategory}` && c.type == ChannelType.GuildCategory
-    );
+    try {
+      const name = interaction.options.getString("name");
+      const guildCategory = "on-demand voice";
+      interaction.client.logger.info(`trying to create voice channel ${name} `);
 
-    interaction.client.logger.info(`trying to create voice channel ${name} in ${category?.name}`, "voiceCreate.js");
-    const channel = await interaction.guild.channels.create({
-      name: name,
-      type: ChannelType.GuildVoice,
-    });
-    interaction.client.logger.info(`created voice channel ${name} in ${category?.name}`, "voiceCreate.js");
-    if (category) await channel.setParent(category.id);
+      const category = interaction.guild.channels.cache.find(
+        (c) =>
+          c.name.toLowerCase() == `${guildCategory}` &&
+          c.type == ChannelType.GuildCategory
+      );
+      interaction.client.logger.error(
+        `error finding category ${guildCategory}`,
+        "voiceCreate.js"
+      );
 
-    await interaction.reply({ content: `Created ${channel} - Click to hop in!`, ephemeral: true });
+      interaction.client.logger.info(`category found?`, "voiceCreate.js");
+      interaction.client.logger.info(
+        `trying to create voice channel ${name} in ${category?.name}[${category?.id}] `,
+        "voiceCreate.js"
+      );
+
+      const channel = await interaction.guild.channels.create({
+        name: name,
+        type: ChannelType.GuildVoice,
+      });
+
+      interaction.client.logger.info(
+        `created voice channel ${name} in ${category?.name}`,
+        "voiceCreate.js"
+      );
+      if (category) await channel.setParent(category.id);
+
+      await interaction.reply({
+        content: `Created ${channel} - Click to hop in!`,
+        ephemeral: true,
+      });
+    } catch (error) {
+      interaction.client.logger.error(error, "voiceCreate.js");
+    }
   },
 };
